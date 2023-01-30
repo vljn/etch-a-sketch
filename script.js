aa9b5f1d7c3dd0c8c2998a13d8d489c8ed8eeff5
vljn <121915880+vljn@users.noreply.github.com>
2023-01-30 19:52:50 +0100

    Add the controls panel to the game

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function fillBoard(board, numberOfSquares) {
+  board.textContent = "";
   for (let i = 0; i < numberOfSquares; i++) {
     let row = document.createElement("div");
     row.classList.add("row");
@@ -9,13 +10,40 @@ function fillBoard(board, numberOfSquares) {
     }
     board.appendChild(row);
   }
+  const squares = document.querySelectorAll(".square");
+
+  squares.forEach((sqr) => sqr.addEventListener("mouseenter", colorSquare));
+
+  sliderH.innerText = `Size: ${numberOfSquares} x ${numberOfSquares}`;
 }
 
 function colorSquare() {
   this.style.backgroundColor = "#000";
 }
 
+function changeInput(e) {
+  const squares = document.querySelectorAll(".square");
+  if (e.target.id === "click") {
+    squares.forEach((sqr) => sqr.addEventListener("click", colorSquare));
+    squares.forEach((sqr) => sqr.removeEventListener("mouseenter", colorSquare));
+  } else {
+    squares.forEach((sqr) => sqr.addEventListener("mouseenter", colorSquare));
+    squares.forEach((sqr) => sqr.removeEventListener("click", colorSquare));
+  }
+}
+
+function fillBoardFromInput(e) {
+  fillBoard(board, e.target.value);
+}
+
 const board = document.querySelector("#board");
-fillBoard(board, 24);
-const squares = document.querySelectorAll(".square");
-squares.forEach((sqr) => sqr.addEventListener("mouseenter", colorSquare));
+const slider = document.querySelector("#slider");
+const sliderH = document.querySelector("#slider-area h3");
+const radios = document.querySelectorAll(".radio-btn");
+const reset = document.querySelector("#reset");
+fillBoard(board, 16);
+slider.addEventListener("input", fillBoardFromInput);
+radios.forEach((btn) => btn.addEventListener("input", changeInput));
+reset.addEventListener("click", () => {
+  fillBoard(board, slider.value);
+});
